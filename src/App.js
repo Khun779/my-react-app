f24405aa204df9a2eb3519f088333cfb64ea5e7e
Khun Yin Tun <khunyintun100@gmail.com>
2024-08-27 22:10:54 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import MyComponent from './components/MyComponent';
+// src/App.js
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,8 @@
+// src/components/MyComponent.js
+import React from 'react';
+
+function MyComponent() {
+  return <h1>Hello, World!</h1>;
+}
+
+export default MyComponent;
